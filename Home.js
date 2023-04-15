4ba7f0fac11bebcd90f87863d90cb43013679cb5
Gonçalo Dias <goncalodias.studio@gmail.com>
2023-04-15 23:40:45 +0100

    :sparkles: Render dynamic content on Home page

--- Home.js
+++ Home.js
@@ -1,5 +1,18 @@
+import ConscientStream from './api.js';
+import Block from './components/Block.js';
+
+const streams = ConscientStream.getStreams();
+
 const Home = () => {
-  return `<h1>Home</h1>`;
+  return `
+    <div>
+      ${streams
+        .map(function (stream) {
+          return Block({ text: stream.text });
+        })
+        .join('')}
+    </div>
+`;
 };
 
 export default Home;
