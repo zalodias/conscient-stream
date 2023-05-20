75e37bc3b815d6827ad92ccd50a7f4d1053ce956
Gonçalo Dias <goncalodias.studio@gmail.com>
2023-05-20 12:16:49 +0100

    :fire: Remove unused icon component and assets

--- components/Icon.js
+++
@@ -1,9 +0,0 @@
-const Icon = async ({ name }) => {
-  const path = `../assets/${name}.svg`;
-  const response = await fetch(path);
-  const icon = await response.text();
-
-  return `<i>${icon}</i>`;
-};
-
-export default Icon;
