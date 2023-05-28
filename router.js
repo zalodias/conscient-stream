9da110f35fc18a2b602b41b45439fe926c1fafbc
Gonçalo Dias <goncalodias.studio@gmail.com>
2023-05-28 13:58:37 +0100

    :sparkles: Add updated app router logic

---
+++ router.js
@@ -0,0 +1,24 @@
+import Home from "./Home.js";
+
+const routes = {
+  "/": Home,
+};
+
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+
+  handleLocation();
+};
+
+const handleLocation = () => {
+  const path = window.location.pathname;
+  const route = routes[path]();
+  document.getElementById("app").innerHTML = route;
+};
+
+window.onpopstate = handleLocation();
+window.route = route;
+
+handleLocation();
